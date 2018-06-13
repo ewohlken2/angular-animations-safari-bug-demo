392c3dbac2491473821cb81cd7a658ea4fecc0bd
Elvis Wohlken <elvis.wohlken@rhythmagency.com>
2018-06-13 14:06:48 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { state, animateChild, stagger, trigger, query, transition, animate, style, group} from '@angular/animations';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('testAnimation', [
+        state('below, none, void', style({
+            opacity: 0,
+            transform: 'translate3d(-100px, 0, 0)',
+        })),
+        state('in, above', style({
+            opacity: 1,
+            transform: 'translate3d(0, 0, 0)',
+        })),
+        transition('* => in', [
+            group([
+                animate(`1000ms ease`, style({
+                    transform: 'translate3d(0, 0, 0)',
+                })),
+                animate(`300ms ease`, style({
+                    opacity: 1,
+                }))
+            ])
+        ])
+    ]),
+    trigger('parentAnimation', [
+      transition('* => in', [
+        group([
+            query('.test', [
+                stagger(200, [
+                    animateChild()
+                ])
+            ]),
+            query('.test2', [
+              stagger(200, [
+                  animateChild()
+              ])
+          ])
+        ])
+      ])
+    ])
+  ]
+})
+export class AppComponent {
+  state;
+
+  ngOnInit() {
+    this.state = 'in';
+  }
+}
